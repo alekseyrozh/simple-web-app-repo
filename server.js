83297b6bac0bf2ae6d8f65005a83645e6aaa4781
Aleksei Rozhnov <alekseyrozh@gmail.com>
2017-11-02 11:43:36 +0000

    1337

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require("http");
 
+// Console will print the message
+var port = process.env.PORT || 1337;
+
 http.createServer(function (request, response) {
     // Send the HTTP header 
     // HTTP Status: 200 : OK
@@ -8,7 +11,7 @@ http.createServer(function (request, response) {
 
     // Send the response body as "Hello World"
     response.end('Hello World\n');
-}).listen(80);
+}).listen(port);
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:80/');
+
+console.log("Server running at http://localhost:%d", port);
